55e2b9209e8a7163f0f649abbd15aa4490684e42
PanDmytryk <dimacapny22@gmail.com>
2024-09-29 18:59:15 +0200

    Add project files.

---
+++ RegistryControlApp/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace RegistryControlApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.regKeysView = new System.Windows.Forms.TreeView();
+            this.valuesListView = new System.Windows.Forms.ListView();
+            this.nameCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.typeCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.valueCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // regKeysView
+            // 
+            this.regKeysView.Location = new System.Drawing.Point(12, 12);
+            this.regKeysView.Name = "regKeysView";
+            this.regKeysView.Size = new System.Drawing.Size(227, 426);
+            this.regKeysView.TabIndex = 0;
+            this.regKeysView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.regKeysView_AfterSelect);
+            // 
+            // valuesListView
+            // 
+            this.valuesListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameCol,
+            this.typeCol,
+            this.valueCol});
+            this.valuesListView.HideSelection = false;
+            this.valuesListView.Location = new System.Drawing.Point(246, 12);
+            this.valuesListView.Name = "valuesListView";
+            this.valuesListView.Size = new System.Drawing.Size(542, 426);
+            this.valuesListView.TabIndex = 1;
+            this.valuesListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // nameCol
+            // 
+            this.nameCol.Text = "Name";
+            this.nameCol.Width = 70;
+            // 
+            // typeCol
+            // 
+            this.typeCol.Text = "Type";
+            this.typeCol.Width = 50;
+            // 
+            // valueCol
+            // 
+            this.valueCol.Text = "Value";
+            this.valueCol.Width = 140;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.valuesListView);
+            this.Controls.Add(this.regKeysView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView regKeysView;
+        private System.Windows.Forms.ListView valuesListView;
+        private System.Windows.Forms.ColumnHeader nameCol;
+        private System.Windows.Forms.ColumnHeader typeCol;
+        private System.Windows.Forms.ColumnHeader valueCol;
+    }
+}
+
---
+++ RegistryControlApp/Form1.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistryControlApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            RegistryKey[] rootKeys =
+            {
+                Registry.ClassesRoot,
+                Registry.LocalMachine,
+                Registry.CurrentUser,
+                Registry.Users,
+                Registry.CurrentConfig
+            };
+
+            for (int i = 0; i < rootKeys.Length; i++)
+            {
+                regKeysView.Nodes.Add(rootKeys[i].Name);
+                InitializeRegistryKeysTree(rootKeys[i], regKeysView.Nodes[i].Nodes);
+            }
+
+            valuesListView.View = View.Details;
+        }
+
+        private void InitializeRegistryKeysTree(RegistryKey rootKey, TreeNodeCollection nodes)
+        {
+            try
+            {
+                string[] keys = rootKey.GetSubKeyNames();
+
+                int nodeIndex = 0;
+                foreach (string key in keys)
+                {
+                    RegistryKey subKey = rootKey.OpenSubKey(key);
+                    nodes.Add(subKey.Name);
+
+                    if (subKey != null)
+                    {
+                        InitializeRegistryKeysTree(subKey, nodes[nodeIndex].Nodes);
+                    }
+
+                    nodeIndex++;
+                }
+            }
+            catch (Exception)
+            {
+                return;
+            }
+        }
+
+        private void regKeysView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            valuesListView.Items.Clear();
+
+            string registryPath = regKeysView.SelectedNode.Text;
+            string[] splittedPath = registryPath.Split('\\');
+
+            RegistryKey rootKey = GetRootKey(splittedPath[0]);
+
+            splittedPath.ToList().RemoveAt(0);
+            string pathWithoutRoot = string.Join("\\", splittedPath).Remove(0, splittedPath[0].Length);
+
+            RegistryKey selectedKey = rootKey.OpenSubKey(pathWithoutRoot) ?? rootKey;
+            string[] valueNames = selectedKey.GetValueNames();
+
+            foreach (string valueName in valueNames)
+            {
+                object value = selectedKey.GetValue(valueName);
+                RegistryValueKind valueKind = selectedKey.GetValueKind(valueName);
+
+                string[] datas = { valueName, valueKind.ToString(), value.ToString() };
+
+                ListViewItem valueListItem = new ListViewItem(datas);
+                valuesListView.Items.Add(valueListItem);
+            }
+        }
+
+        private RegistryKey GetRootKey(string name)
+        {
+            switch (name)
+            {
+                case "HKEY_CLASSES_ROOT":
+                    return Registry.ClassesRoot;
+                case "HKEY_LOCAL_MACHINE":
+                    return Registry.LocalMachine;
+                case "HKEY_CURRENT_USER":
+                    return Registry.CurrentUser;
+                case "HKEY_USERS":
+                    return Registry.Users;
+                case "HKEY_CURRENT_CONFIG":
+                    return Registry.CurrentConfig;
+                default:
+                    return Registry.LocalMachine;
+            }
+        }
+    }
+}
